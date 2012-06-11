cfdb3ba7366ca993618a11c095325959918b2f92
malte <malte.pickhan@gmail.com>
2012-06-11 22:56:01 +0200

    Initial maven commit

---
+++ src/main/java/de/rupi/predictio/Predictio.java
@@ -0,0 +1,19 @@
+package de.rupi.predictio;
+
+import org.slf4j.Logger;
+
+
+
+
+public class Predictio {
+
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
